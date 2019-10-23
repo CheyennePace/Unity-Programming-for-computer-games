f309dd181a40b559219a928b685e868c0eb6db04
CheyennePace <CheyennePace@users.noreply.github.com>
2019-10-23 11:30:58 +0200

    collision of the block,wall colliders
    made the block disapear and created wall colliders

--- BlockBreaker/Assets/Scripts/ball.cs
+++ BlockBreaker/Assets/Scripts/ball.cs
@@ -8,6 +8,8 @@ public class ball : MonoBehaviour
 
     Vector2 paddleToBallDistance;
 
+    bool hasStarated = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +22,25 @@ public class ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 paddlePosition = myPaddle.transform.position; 
+        if(!hasStarated) //hasStarted == false
+        {
+            LockBallToPaddle();
 
+            if(Input.GetMouseButtonDown (0)) //left click
+            {
+                hasStarated = true;
+                //shoot the ball
+                GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+            }
+        }
+        
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = myPaddle.transform.position; 
         //set the Ball Position = Paddle Postion + paddleToBallPosition
         this.transform.position = paddlePosition + paddleToBallDistance;
     }
+    
 }
